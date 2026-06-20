5aaa5a284f8f4844bb0c0df123892104872da081
bhaktipwar97 <pawarbhakti41@gmail.com>
2026-06-20 19:22:47 +0530

    fixed api url

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,26 +1,17 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL:
-    "http://localhost:5000/api",
+  baseURL: "https://e-commerceweb-5b2y.onrender.com/api",
 });
 
-API.interceptors.request.use(
-  (req) => {
+API.interceptors.request.use((req) => {
+  const token = localStorage.getItem("token");
 
-    const token =
-      localStorage.getItem(
-        "token"
-      );
-
-    if (token) {
-
-      req.headers.Authorization =
-        `Bearer ${token}`;
-    }
-
-    return req;
+  if (token) {
+    req.headers.Authorization = `Bearer ${token}`;
   }
-);
+
+  return req;
+});
 
 export default API;
