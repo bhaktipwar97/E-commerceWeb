3cd75359508614d2ecf2a29c3642a4b907d795a4
bhaktipwar97 <pawarbhakti41@gmail.com>
2026-06-28 13:58:35 +0530

    Initial commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -39,7 +39,7 @@ const App = () => {
 
         <Route path="/cart" element={<Cart />} />
 
-        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/Checkout" element={<Checkout />} />
 
         <Route path="/wishlist" element={<Wishlist />} />
 
